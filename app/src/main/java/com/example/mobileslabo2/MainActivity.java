2c424447f30112733ef457ad095e83f78f111a0b
Carlos Ruiz <00095417@uca.edu.sv>
2019-03-16 05:16:10 -0600

    problema de compilación resuelto

--- app/src/main/java/com/example/mobileslabo2/MainActivity.java
+++ app/src/main/java/com/example/mobileslabo2/MainActivity.java
@@ -5,36 +5,36 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private LinearLayout mLinearLayout;
-    private Button btn = findViewById(R.id.btn1);
-    private TextView text1 = findViewById(R.id.text1);
-    private EditText edit1 = findViewById(R.id.user_name1);
-    private EditText edit2 = findViewById(R.id.pass);
+    private Button btn;
+    private EditText edit_name, edit_pass;
+    private TextView text1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mLinearLayout = findViewById(R.id.ll_1);
+        btn = findViewById(R.id.Btn1);
+        edit_name = findViewById(R.id.User_text);
+        edit_pass = findViewById(R.id.Pass_text);
+        text1 = findViewById(R.id.Text1);
 
         btn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                text1.setText(edit1.getText());
+                text1.setText(edit_name.getText());
             }
         });
 
-        btn.setOnLongClickListener(new View.OnLongClickListener(){
-
+        btn.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                text1.setText(edit2.getText());
+                text1.setText(edit_pass.getText());
                 return false;
             }
         });
